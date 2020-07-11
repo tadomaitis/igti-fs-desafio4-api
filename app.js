18c056ef3510ac46a72b06a82e06aa4f0c3955ea
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2020-07-11 16:16:51 -0300

    Changes cors port

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: process.env.CORS_ORIGIN,
   })
 );
 
